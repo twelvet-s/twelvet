a085b6044dee43b9b1f8bdab94181548a4885f2a
twelvet <2471835953@qq.com>
2020-12-09 14:35:23 +0800

    修复bug

--- main/twelvet-framework/twelvet-framework-core/src/main/java/com/twelvet/framework/core/application/domain/AjaxResult.java
+++ main/twelvet-framework/twelvet-framework-core/src/main/java/com/twelvet/framework/core/application/domain/AjaxResult.java
@@ -11,37 +11,43 @@ import java.util.LinkedHashMap;
  * @WebSite www.twelvet.cn
  * @Description: 统一操作信息实体
  */
-public class AjaxResult extends LinkedHashMap<String, Object>
-{
+public class AjaxResult extends LinkedHashMap<String, Object> {
+
     private static final long serialVersionUID = 1L;
 
-    /** 状态码 */
+    /**
+     * 状态码
+     */
     public static final String CODE_TAG = "code";
 
-    /** 返回内容 */
+    /**
+     * 返回内容
+     */
     public static final String MSG_TAG = "msg";
 
-    /** 数据对象 */
+    /**
+     * 数据对象
+     */
     public static final String DATA_TAG = "data";
 
-    /** 访问路径 */
+    /**
+     * 访问路径
+     */
     public static final String PATH_TAG = "path";
 
     /**
      * 初始化一个新创建的 AjaxResult 对象，使其表示一个空消息。
      */
-    public AjaxResult()
-    {
+    public AjaxResult() {
     }
 
     /**
      * 初始化一个新创建的 AjaxResult 对象
      *
      * @param code 状态码
-     * @param msg 返回内容
+     * @param msg  返回内容
      */
-    public AjaxResult(int code, String msg)
-    {
+    public AjaxResult(int code, String msg) {
         super.put(CODE_TAG, code);
         super.put(MSG_TAG, msg);
     }
@@ -50,15 +56,13 @@ public class AjaxResult extends LinkedHashMap<String, Object>
      * 初始化一个新创建的 AjaxResult 对象
      *
      * @param code 状态码
-     * @param msg 返回内容
+     * @param msg  返回内容
      * @param data 数据对象
      */
-    public AjaxResult(int code, String msg, Object data)
-    {
+    public AjaxResult(int code, String msg, Object data) {
         super.put(CODE_TAG, code);
         super.put(MSG_TAG, msg);
-        if (!TWTUtils.isEmpty(data))
-        {
+        if (!TWTUtils.isEmpty(data)) {
             super.put(DATA_TAG, data);
         }
     }
@@ -68,8 +72,7 @@ public class AjaxResult extends LinkedHashMap<String, Object>
      *
      * @return 成功消息
      */
-    public static AjaxResult success()
-    {
+    public static AjaxResult success() {
         return AjaxResult.success("操作成功");
     }
 
@@ -78,8 +81,7 @@ public class AjaxResult extends LinkedHashMap<String, Object>
      *
      * @return 成功消息
      */
-    public static AjaxResult success(Object data)
-    {
+    public static AjaxResult success(Object data) {
         return AjaxResult.success("操作成功", data);
     }
 
@@ -89,20 +91,18 @@ public class AjaxResult extends LinkedHashMap<String, Object>
      * @param msg 返回内容
      * @return 成功消息
      */
-    public static AjaxResult success(String msg)
-    {
+    public static AjaxResult success(String msg) {
         return AjaxResult.success(msg, null);
     }
 
     /**
      * 返回成功消息
      *
-     * @param msg 返回内容
+     * @param msg  返回内容
      * @param data 数据对象
      * @return 成功消息
      */
-    public static AjaxResult success(String msg, Object data)
-    {
+    public static AjaxResult success(String msg, Object data) {
         return new AjaxResult(HttpStatus.OK.value(), msg, data);
     }
 
@@ -111,8 +111,7 @@ public class AjaxResult extends LinkedHashMap<String, Object>
      *
      * @return AjaxResult
      */
-    public static AjaxResult error()
-    {
+    public static AjaxResult error() {
         return AjaxResult.error("操作失败");
     }
 
@@ -122,20 +121,18 @@ public class AjaxResult extends LinkedHashMap<String, Object>
      * @param msg 返回内容
      * @return 警告消息
      */
-    public static AjaxResult error(String msg)
-    {
+    public static AjaxResult error(String msg) {
         return AjaxResult.error(msg, null);
     }
 
     /**
      * 返回错误消息
      *
-     * @param msg 返回内容
+     * @param msg  返回内容
      * @param data 数据对象
      * @return 警告消息StringUtil
      */
-    public static AjaxResult error(String msg, Object data)
-    {
+    public static AjaxResult error(String msg, Object data) {
         return new AjaxResult(HttpStatus.INTERNAL_SERVER_ERROR.value(), msg, data);
     }
 
@@ -143,11 +140,10 @@ public class AjaxResult extends LinkedHashMap<String, Object>
      * 返回错误消息
      *
      * @param code 状态码
-     * @param msg 返回内容
+     * @param msg  返回内容
      * @return 警告消息
      */
-    public static AjaxResult error(int code, String msg)
-    {
+    public static AjaxResult error(int code, String msg) {
         return new AjaxResult(code, msg, null);
     }
 }
