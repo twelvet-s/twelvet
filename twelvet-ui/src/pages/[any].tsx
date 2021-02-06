d8728e97de57042965df9787d2d316b9c6400684
twelvet <2471835953@qq.com>
2021-02-06 17:06:30 +0800

    添加404向导

--- twelvet-ui/src/pages/[any].tsx
+++ twelvet-ui/src/pages/[any].tsx
@@ -1,6 +1,6 @@
-import { Button, Result } from 'antd';
-import React from 'react';
-import { history } from 'umi';
+import { Button, Result } from 'antd'
+import React from 'react'
+import { history } from 'umi'
 
 const NoFoundPage: React.FC<{}> = () => {
 	return (
@@ -9,15 +9,20 @@ const NoFoundPage: React.FC<{}> = () => {
 			title="404"
 			subTitle="Sorry, the page you visited does not exist."
 			extra={
-				<Button type="primary" onClick={() => history.push('/')}>
-					Back Home
-      			</Button>
+				[
+					<Button type="primary" onClick={() => history.goBack()}>
+						返回上一页
+					</Button>,
+					<Button type="primary" onClick={() => history.push('/')}>
+						Back Home
+					</Button>
+				]
 			}
 		/>
 	)
 }
 
 // 修复umi bug兼容404
-NoFoundPage.path = undefined;
+NoFoundPage.path = undefined
 
-export default NoFoundPage;
+export default NoFoundPage
