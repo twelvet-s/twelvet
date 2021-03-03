1a42bdd9c99de45ed6f9bfa13434f6b7caaeae09
L <2471835953@qq.com>
2021-03-03 13:55:45 +0800

    优化打包镜像大小

--- main/twelvet-gateway/src/main/java/com/twelvet/config/GatewayConfig.java
+++ main/twelvet-gateway/src/main/java/com/twelvet/config/GatewayConfig.java
@@ -2,11 +2,14 @@ package com.twelvet.config;
 
 import com.alibaba.csp.sentinel.adapter.gateway.sc.SentinelGatewayFilter;
 import com.twelvet.handler.SentinelFallbackHandler;
+import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilderCustomizer;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
+
+import java.util.TimeZone;
 
 /**
  * @author twelvet
@@ -14,18 +17,24 @@ import org.springframework.core.annotation.Order;
  * @Description: 网关限流配置
  */
 @Configuration
-public class GatewayConfig
-{
+public class GatewayConfig {
+
+    /**
+     * 时区配置
+     */
+    @Bean
+    public Jackson2ObjectMapperBuilderCustomizer jacksonObjectMapperCustomization() {
+        return jacksonObjectMapperBuilder -> jacksonObjectMapperBuilder.timeZone(TimeZone.getDefault());
+    }
+
     @Bean
     @Order(Ordered.HIGHEST_PRECEDENCE)
-    public SentinelFallbackHandler sentinelGatewayExceptionHandler()
-    {
+    public SentinelFallbackHandler sentinelGatewayExceptionHandler() {
         return new SentinelFallbackHandler();
     }
 
     @Order(-1)
-    public GlobalFilter sentinelGatewayFilter()
-    {
+    public GlobalFilter sentinelGatewayFilter() {
         return new SentinelGatewayFilter();
     }
 }
