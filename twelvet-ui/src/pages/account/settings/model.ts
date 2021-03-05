51e83aca877043a96629b4f3ec24470441e7ad1c
L <2471835953@qq.com>
2021-03-05 09:37:06 +0800

    删除无用方法

--- twelvet-ui/src/pages/account/settings/model.ts
+++ twelvet-ui/src/pages/account/settings/model.ts
@@ -1,11 +1,9 @@
 import { Effect, Reducer } from 'umi';
-import { CurrentUser, GeographicItemType } from './data.d';
-import { queryCity, queryCurrent, queryProvince, query as queryUsers } from './service';
+import { CurrentUser } from './data.d';
+import { queryCurrent } from './service';
 
 export interface ModalState {
     currentUser?: Partial<CurrentUser>;
-    province?: GeographicItemType[];
-    city?: GeographicItemType[];
     isLoading?: boolean;
 }
 
@@ -14,16 +12,9 @@ export interface ModelType {
     state: ModalState;
     effects: {
         fetchCurrent: Effect;
-        fetch: Effect;
-        fetchProvince: Effect;
-        fetchCity: Effect;
     };
     reducers: {
         saveCurrentUser: Reducer<ModalState>;
-        changeNotifyCount: Reducer<ModalState>;
-        setProvince: Reducer<ModalState>;
-        setCity: Reducer<ModalState>;
-        changeLoading: Reducer<ModalState>;
     };
 }
 
@@ -32,44 +23,17 @@ const Model: ModelType = {
 
     state: {
         currentUser: {},
-        province: [],
-        city: [],
         isLoading: false,
     },
 
     effects: {
-        *fetch(_, { call, put }) {
-            const response = yield call(queryUsers);
-            yield put({
-                type: 'save',
-                payload: response,
-            });
-        },
         *fetchCurrent(_, { call, put }) {
             const response = yield call(queryCurrent);
             yield put({
                 type: 'saveCurrentUser',
                 payload: response.data.user,
             });
-        },
-        *fetchProvince(_, { call, put }) {
-            yield put({
-                type: 'changeLoading',
-                payload: true,
-            });
-            const response = yield call(queryProvince);
-            yield put({
-                type: 'setProvince',
-                payload: response,
-            });
-        },
-        *fetchCity({ payload }, { call, put }) {
-            const response = yield call(queryCity, payload);
-            yield put({
-                type: 'setCity',
-                payload: response,
-            });
-        },
+        }
     },
 
     reducers: {
@@ -78,35 +42,7 @@ const Model: ModelType = {
                 ...state,
                 currentUser: action.payload || {},
             };
-        },
-        changeNotifyCount(state = {}, action) {
-            return {
-                ...state,
-                currentUser: {
-                    ...state.currentUser,
-                    notifyCount: action.payload.totalCount,
-                    unreadCount: action.payload.unreadCount,
-                },
-            };
-        },
-        setProvince(state, action) {
-            return {
-                ...state,
-                province: action.payload,
-            };
-        },
-        setCity(state, action) {
-            return {
-                ...state,
-                city: action.payload,
-            };
-        },
-        changeLoading(state, action) {
-            return {
-                ...state,
-                isLoading: action.payload,
-            };
-        },
+        }
     },
 };
 
