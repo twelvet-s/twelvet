37ec0b5f1a61308022261df1a7aa50d9baa259f9
L <2471835953@qq.com>
2021-03-12 12:28:02 +0800

    多数据源

--- main/sql/twelvet_nacos.sql
+++ main/sql/twelvet_nacos.sql
@@ -1,19 +1,3 @@
-/*
- Navicat Premium Data Transfer
-
- Source Server         : localhost
- Source Server Type    : MySQL
- Source Server Version : 50726
- Source Host           : localhost:3306
- Source Schema         : twelvet_nacos
-
- Target Server Type    : MySQL
- Target Server Version : 50726
- File Encoding         : 65001
-
- Date: 12/03/2021 12:19:21
-*/
-
 SET NAMES utf8mb4;
 SET FOREIGN_KEY_CHECKS = 0;
 
